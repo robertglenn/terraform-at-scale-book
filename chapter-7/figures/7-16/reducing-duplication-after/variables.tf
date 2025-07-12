f73a8029ea719bb0452d006a8179e84e085100d9
Robert Glenn <robertcharlesglenn@gmail.com>
2025-07-11 19:45:59 -0700

    fixing the variable construction on the after example

--- chapter-7/figures/7-16/reducing-duplication-after/variables.tf
+++ chapter-7/figures/7-16/reducing-duplication-after/variables.tf
@@ -1,9 +1,9 @@
 
 variable "snet_configs" {
     description = "A list of subnet information."
-    type        = object({
+    type        = list(object({
         name   = string
         cidr   = string
         region = string
-    })
+    }))
 }
